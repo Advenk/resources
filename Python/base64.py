de14bb5a0574dc3875d2431c94edd8c9c57dfb70
Advenk <36237258+Advenk@users.noreply.github.com>
2022-08-05 19:40:34 -0700

    Update base64.py

--- Python/base64.py
+++ Python/base64.py
@@ -7,8 +7,8 @@ import base64
 
 print("This script will help you encode or decrypt base64 strings\n")
 
-data = input("Enter the data you would like to encode or decrypt.\n").replace('\n', '') # .encode() needed to convert input to bytes like onject
-data.encode()
+data = input("Enter the data you would like to encode or decrypt.\n").replace('\n', '') 
+data.encode() # .encode() needed to convert input to bytes like onject
 
 option = int(input("Are we decoding or encoding? (1 or 2)\n 1.Encoding\n 2.Decoding\n" ))
 
