19abd2b5df17bad84f639824a4634262d1df59c9
Jeff K <36237258+Advenk@users.noreply.github.com>
2022-08-18 04:50:48 -0700

    Update BulkGroupManagerChange.ps1

--- PowerShell/BulkGroupManagerChange.ps1
+++ PowerShell/BulkGroupManagerChange.ps1
@@ -1,10 +1,8 @@
-# By Jeff K.
 # This script takes a list and a given Active Directory username and will set all given groups to the specified user.
-# The manager's username must be updated on $manager
 # Needs to be pointed at a list of groups for $inputFile
 # Must be a simple list of groups this script assumes there is no header
 
-$manager = Get-ADUser -Identity "user"
+$manager = Get-ADUser -Identity "user" # The manager's username must be updated here
 $inputFile = 'C:\paths\to\list\example.csv'
 $Groups = ForEach ($GroupName in $inputFile) {Get-Content $inputFile}
 ForEach ($Group in $Groups){
